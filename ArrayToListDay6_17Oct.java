c8ef92551ff676513a1c26e6feed7be6bef1c7d2
nangoyal <115457952+nangoyal@users.noreply.github.com>
2022-10-24 12:09:55 +0530

    Add files via upload

---
+++ ArrayToListDay6_17Oct.java
@@ -0,0 +1,36 @@
+package capgTraining;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToListDay6_17Oct {
+		
+	// Generic function to convert an Array to List
+		public static <T> List<T> convertArrayToList(T array[])
+		{
+
+			// Create the List by passing the Array
+			// as parameter in the constructor
+			List<T> list = Arrays.asList(array);
+
+			// Return the converted List
+			return list;
+		}
+
+		public static void main(String args[])
+		{
+			// Create an Array
+			String array[]
+				= { "Geeks", "forGeeks", "A computer Portal" };
+
+			// Print the Array
+			System.out.println("Array: "
+							+ Arrays.toString(array));
+
+			// convert the Array to List
+			List<String> list = convertArrayToList(array);
+
+			// Print the List
+			System.out.println("List: " + list);
+	}
+}
