169395743943a0193aae077375180840d3c79177
nangoyal <115457952+nangoyal@users.noreply.github.com>
2022-10-24 12:13:16 +0530

    Add files via upload

---
+++ InvalidAgeExceptionDay4_13Oct.java
@@ -0,0 +1,12 @@
+package capgTraining;
+//Custom Exception -- For eg: InvalidAgeException
+
+public class InvalidAgeExceptionDay4_13Oct extends RuntimeException {
+	InvalidAgeExceptionDay4_13Oct (String msg){
+		super(msg);
+	}
+	
+	
+	
+
+}
